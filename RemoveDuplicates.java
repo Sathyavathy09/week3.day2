d41e4b8ebb6316960f1d7b312bf0b86f338c92c5
Sathyavathy09 <93446772+Sathyavathy09@users.noreply.github.com>
2021-11-16 21:25:17 -0800

    Add files via upload

---
+++ RemoveDuplicates.java
@@ -0,0 +1,24 @@
+package week3.day2;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class RemoveDuplicates {
+
+	public static void main(String[] args) {
+
+		String text = "We learn java basics as part of java sessions in java week1";
+
+		String[] strgs = text.split(" ");
+
+		Set<String> textSet = new LinkedHashSet<String>();
+
+		for (String eachValue : strgs) {
+			textSet.add(eachValue);
+
+		}
+		System.out.println(textSet);
+
+	}
+
+}
